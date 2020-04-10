05741bfb38756ef3a479f4f1e0c58b52c475fde4
Balaji <pbalajieee@gmail.com>
2020-04-10 15:00:30 +0530

    Initial Commit

---
+++ LoaderUIButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoaderUIButton
+//
+//  Created by Sabari on 10/04/20.
+//  Copyright © 2020 Sabari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
